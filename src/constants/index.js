c7f25a642f463d3b4625a491b55878a04c5649dc
xmunene <vmunene42@gmail.com>
2025-08-26 23:26:17 +0300

    Button Bug Fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,10 +29,10 @@ const navLinks = [
   ];
   
   const counterItems = [
-    { value: 15, suffix: "+", label: "Years of Experience" },
-    { value: 200, suffix: "+", label: "Satisfied Clients" },
-    { value: 108, suffix: "+", label: "Completed Projects" },
-    { value: 90, suffix: "%", label: "Client Retention Rate" },
+    { value: 2, suffix: "+", label: "Years of Experience" },
+    { value: 3, suffix: "+", label: "Satisfied Clients" },
+    { value: 10, suffix: "+", label: "Completed Projects" },
+    { value: 95, suffix: "%", label: "Client Retention Rate" },
   ];
   
   const logoIconsList = [
