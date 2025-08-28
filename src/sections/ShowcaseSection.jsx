afd3b2186dd2d1f4cd5b4ad21e787069afe6157b
xmunene <vmunene42@gmail.com>
2025-08-29 00:19:21 +0300

    GSAP Animation

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,12 +1,50 @@
 import React from 'react';
+import { useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ShowcaseSection = () => {
+    const sectionRef = useRef(null);
+    const project1Ref = useRef(null);
+    const project2Ref = useRef(null);
+    const project3Ref = useRef(null);
+
+    useGSAP(() => {
+        const projects = [project1Ref.current, project2Ref.current, project3Ref.current];
+        projects.forEach((card, index) => {
+            gsap.fromTo(
+                card,
+                {
+                    y: 50, opacity: 0 
+                },
+                {
+                    y: 0,
+                    opacity: 1,
+                    duration: 1,
+                    delay: 0.3 * (index + 1),
+                    scrollTrigger:{
+                        trigger: card,
+                        start: 'top bottom-=100',
+    
+                    }
+                }
+            )
+        })
+        gsap.fromTo(
+            sectionRef.current, 
+            { opacity: 0 },
+            { opacity: 1, duration: 1.5 })
+    }, []);
+
     return(
-        <div id="work" className="app-showcase">
+        <section id="work" ref={sectionRef}className="app-showcase">
             <div className="w-full">
                 <div className="showcaselayout">
                     {/* Left side */}
-                    <div className="first-project-wrapper">
+                    <div className="first-project-wrapper" ref={project1Ref}>
                         <div className="image-wrapper">
                         <img src="/images/project1.png" alt="ticketsawa" />
                         </div>
@@ -20,13 +58,13 @@ const ShowcaseSection = () => {
 
                     {/* Right Side */}
                     <div className="project-list-wrapper overflow">
-                        <div className="project">
+                        <div className="project" ref={project2Ref}>
                             <div className="image-wrapper bg-[#e6dbff]">
                                 <img src="/images/project2.png" alt="3D Portfolio" />
                             </div>
                             <h2>3D Portfolio with Three.JS</h2>
                         </div>
-                        <div className="project">
+                        <div className="project" ref={project3Ref}>
                             <div className="image-wrapper bg-[#dbecff]">
                                 <img src="/images/project4.png" alt="Figma Design" />
                             </div>
@@ -35,7 +73,7 @@ const ShowcaseSection = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 export default ShowcaseSection
