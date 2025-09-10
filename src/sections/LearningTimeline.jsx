0eeb6b20370a53b69464dc830fa7701f464d6012
xmunene <vmunene42@gmail.com>
2025-09-10 13:03:23 +0300

    Learning Pathway Added

--- src/sections/LearningTimeline.jsx
+++ src/sections/LearningTimeline.jsx
@@ -2,8 +2,59 @@ import React from 'react'
 import TitleHeader from '../components/TitleHeader.jsx'
 import { expCards } from '../constants/index.js'
 import GlowCard from '../components/GlowCard.jsx'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger);
+
 
 const LearningTimeline = () => {
+    useGSAP(() => {
+        gsap.utils.toArray('.timeline-card').forEach((card) => {
+            gsap.from(card, {
+                xPercent: -100,
+                opacity: 0,
+                transformOrigin: 'left left',
+                duration: 1,
+                ease: 'power2.inOut',
+                scrollTrigger: {
+                    trigger: card,
+                    start: 'top 80%'
+                }
+            })
+        })
+
+        gsap.to('.timeline', {
+            transformOrigin: 'bottom bottom',
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '.timeline',
+                start: 'top center',
+                end: '70% center',
+                onUpdate: (self) => {
+                    gsap.to('.timeline', {
+                        scaleY: 1 - self.progress,
+                    })
+                }
+            },
+        })
+
+        gsap.utils.toArray('.expText').forEach((text) => {
+            gsap.from(text, {
+                xPercent: 0,
+                opacity: 0,
+                duration: 1,
+                ease: 'power2.inOut',
+                scrollTrigger: {
+                    trigger: text,
+                    start: 'top 60%'
+                }
+            })
+        })
+
+    }, []);
+
     return (
         <section id="experience" className="w-full md:mt-40 mt-20 section-padding xl:px-0">
             <div className="w-full h-full md:px-20 px-5">
@@ -18,6 +69,35 @@ const LearningTimeline = () => {
                                             <img src={card.imgPath} alt={card.title} />
                                         </div>
                                    </GlowCard>
+                                </div>
+                                <div className="xl:w-4/6">
+                                    <div className="flex items-start">
+                                        <div className="timeline-wrapper">
+                                            <div className="timeline" />
+                                            <div className ="gradient-line w-1 h-full" />
+                                        </div>
+                                        <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+                                            <div className="timeline-logo">
+                                                <img src={card.logoPath} alt="logo" />
+                                            </div>
+                                            <div>
+                                                <h1 className="font-semibold text-3xl">{card.title}</h1>
+                                                <p className="my-5 text-white-50">
+                                                    📅{card.date}
+                                                </p>
+                                                <p className="text-[#839cb5] italic">
+                                                    Responsibilities
+                                                </p>
+                                                <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
+                                                    {card.responsibilities.map((responsibility) => (
+                                                        <li key={responsibility} className="text-lg">
+                                                            {responsibility}
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         ))}
